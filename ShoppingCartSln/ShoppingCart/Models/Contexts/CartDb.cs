5a2844ecf762dae807e101a3d7ccb4b0c024e27f
Bob Bobga Nti <bobganti@yahoo.com>
2020-10-24 02:05:11 +0100

    EF, Db, Context class

---
+++ ShoppingCartSln/ShoppingCart/Models/Contexts/CartDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models.Contexts
+{
+    public class CartDb : DbContext
+    {
+        public CartDb() : base("CartDb")
+        { }
+
+
+    }
+}
