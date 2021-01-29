68b0a9f075d14b922027ced24ec61e58ed8ac95b
Bob Bobga Nti <bobganti@yahoo.com>
2021-01-29 02:23:19 +0000

    Clients Area

---
+++ ShoppingCartSln/ShoppingCart/Areas/Clients/ClientsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ShoppingCart.Areas.Admin
+{
+    public class ClientsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Admin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Clients",
+                "Clients/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
